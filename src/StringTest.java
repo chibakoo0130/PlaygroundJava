189115d66dd4d1b211d7e4247c0a0f95afad5d24
Kotaro Chiba <kohtarocb@gmail.com>
2021-11-24 22:39:01 +0900

    add StringTest

---
+++ src/StringTest.java
@@ -0,0 +1,47 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+@RunWith(Enclosed.class)
+public class StringTest {
+
+    public static class trimテスト {
+	@Test
+	public void trimされる() {
+	    assertThat("".trim(), is(""));
+	    assertThat(" ".trim(), is(""));
+	    assertThat(" _".trim(), is("_"));
+	    assertThat("1234567890".trim(), is("1234567890"));
+	    assertThat("_ ".trim(), is("_"));
+	    assertThat(" _ ".trim(), is("_"));
+	}
+	@Test
+	public void trimされない() {
+	    // 全角スペースはtrimされない
+	    assertThat("　".trim(), is("　"));
+	    assertThat("　trim　".trim(), is("　trim　"));
+	}
+    }
+
+    public static class codePointAtテスト{
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void 空文字に対してはエラー() {
+	    "".codePointAt(0);
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void 文字列より大きいインデックスを指定するとエラー() {
+	    "1".codePointAt(2);
+	}
+
+	@Test
+	public void codePointAtされる() {
+	    assertThat(" ".codePointAt(0), is(32));
+	    assertThat("東".codePointAt(0), is(26481));
+	}
+    }
+}
