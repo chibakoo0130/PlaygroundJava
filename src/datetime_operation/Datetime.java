639504f69d20e3ab4ded8e6b6200d98a4b5e0d43
Kotaro Chiba <kohtarocb@gmail.com>
2022-01-17 20:50:11 +0900

    add test parse

--- src/datetime_operation/Datetime.java
+++ src/datetime_operation/Datetime.java
@@ -2,12 +2,12 @@ package datetime_operation;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertThrows;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
 @RunWith(Enclosed.class)
 public class Datetime {
 
-    public static class DateAndTimeのテスト {
+    public static class ofのテスト {
 
         @Test
         public void _2020年1月1日の日付を生成する() {
@@ -26,8 +26,8 @@ public class Datetime {
 
         @Test
         public void _10時20分30秒400の時刻を生成する() {
-            LocalTime time = LocalTime.of(10, 20, 30);
-            assertThat(DateTimeFormatter.ofPattern("HH時mm分ss秒").format(time), is("10時20分30秒"));
+            LocalTime time = LocalTime.of(10, 20, 30, 400_000_000);
+            assertThat(DateTimeFormatter.ofPattern("HH時mm分ss秒400").format(time), is("10時20分30秒400"));
         }
 
         @Test
@@ -35,5 +35,52 @@ public class Datetime {
             LocalDateTime datetime = LocalDateTime.of(2021, 1, 16, 22, 13, 10);
             assertThat(DateTimeFormatter.ofPattern("yyyy年MM月dd日HH時mm分ss秒").format(datetime), is("2021年01月16日22時13分10秒"));
         }
+    }
+
+    public static class parseのテスト {
+
+        @Test
+        public void _2020年1月1日の日付を生成する() {
+            LocalDate date = LocalDate.parse("2020-01-01");
+            assertThat(date, is(LocalDate.of(2020, 1, 1)));
+        }
+
+        @Test(expected = DateTimeParseException.class)
+        public void スラッシュ区切りの日付でparseする() {
+            LocalDate.parse("2020/01/01");
+        }
+
+        @Test(expected = DateTimeParseException.class)
+        public void 区切りなしの日付でparseする() {
+            LocalDate.parse("20200101");
+        }
+
+        @Test(expected = DateTimeParseException.class)
+        public void 日が欠けた日付でparseする() {
+            LocalDate.parse("2020-01");
+        }
+
+        @Test
+        public void _10時20分30秒400の時刻を生成する() {
+            LocalTime time = LocalTime.parse("10:20:30.400");
+            assertThat(time, is(LocalTime.of(10, 20, 30, 400_000_000)));
+        }
+
+        @Test(expected = DateTimeParseException.class)
+        public void コンマ区切りの時刻でparseする() {
+            LocalTime.parse("10.20.30.400");
+        }
+
+        @Test
+        public void _2021年1月16日22時13分10秒456の日時を生成する() {
+            LocalDateTime dateTime = LocalDateTime.parse("2021-01-16T22:13:10.456");
+            assertThat(dateTime, is(LocalDateTime.of(2021, 1, 16, 22, 13, 10, 456_000_000)));
+        }
+
+        @Test(expected = DateTimeParseException.class)
+        public void Tを脱字した日時でparseする() {
+            LocalDateTime.parse("2021-01-1622:13:10.456");
+        }
+
     }
 }
